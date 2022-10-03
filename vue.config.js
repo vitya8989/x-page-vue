f7a8a5732f861527cf6f049200b5cecf3dcbb977
Victor <vitya898989@gmail.com>
2022-10-03 12:56:10 +0300

    fix path

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/x-page-vue/',
+  publicPath: '/',
   pages: {
     index: {
       entry: 'src/main.js',
